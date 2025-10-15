3aa24616f3c7a5a33fb6bded4e95570335ffa126
Yurii Kit <insidelkit@gmail.com>
2025-10-15 13:34:39 +0300

    init

---
+++ app/notes/page.tsx
@@ -0,0 +1,43 @@
+// "use server";!!!!!!!!!!!!!!!!
+import { getNotes } from '@/lib/api';
+import NoteList from '@/components/NoteList/NoteList';
+
+const Notes = async () => {
+  const response = await getNotes();
+
+  return (
+    <section>
+      <h1>Notes List</h1>
+      {response?.notes?.length > 0 && <NoteList notes={response.notes} />}
+    </section>
+  );
+};
+
+export default Notes;
+
+// 'use client';
+
+// import { useState } from 'react';
+// import NoteList from '@/components/NoteList/NoteList';
+// import { getNotes, Note } from '@/lib/api';
+
+// const Notes = () => {
+//   const [notes, setNotes] = useState<Note[]>([]);
+
+//   const handleClick = async () => {
+//     const response = await getNotes();
+//     if (response?.notes) {
+//       setNotes(response.notes);
+//     }
+//   };
+
+//   return (
+//     <section>
+//       <h1>Notes List</h1>
+//       <button onClick={handleClick}>Get my notes</button>
+//       {notes.length > 0 && <NoteList notes={notes} />}
+//     </section>
+//   );
+// };
+
+// export default Notes;
