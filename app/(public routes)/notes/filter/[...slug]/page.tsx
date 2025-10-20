b7295f4baad8bbb7d97bdcb3697c1776cd4474e5
Yurii Kit <insidelkit@gmail.com>
2025-10-20 17:56:40 +0300

    fixed fetcn notes

--- app/(public routes)/notes/filter/[...slug]/page.tsx
+++ app/(public routes)/notes/filter/[...slug]/page.tsx
@@ -4,18 +4,26 @@ import { getNotes } from '@/lib/api';
 import NoteList from '@/components/NoteList/NoteList';
 
 type Props = {
-  params: Promise<{ slug: string[] }>;
+  params: Promise<{ slug?: string[] }>;
 };
 
 const NotesByCategory = async ({ params }: Props) => {
   const { slug } = await params;
-  const category = slug[0] === 'all' ? undefined : slug[0];
+
+  // безпечна обробка порожнього slug
+  const category =
+    slug && slug.length > 0 && slug[0] !== 'all' ? slug[0] : undefined;
+
   const response = await getNotes(category);
 
   return (
     <div>
       <h1>Notes List</h1>
-      {response?.notes?.length > 0 && <NoteList notes={response.notes} />}
+      {response?.notes?.length > 0 ? (
+        <NoteList notes={response.notes} />
+      ) : (
+        <p>No notes found.</p>
+      )}
     </div>
   );
 };
